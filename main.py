1cb8f4a39f282a7a73242e4d22af2f7f00e7cb4e
Semih ERTAN <161311067+semihertan@users.noreply.github.com>
2024-07-22 13:12:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from customtkinter import *
-from PIL import Image, ImageTk
-from tkinter import Label, StringVar, IntVar, messagebox
+from PIL import Image
+from tkinter import StringVar, IntVar, messagebox
 from tkcalendar import Calendar
 from datetime import datetime, timedelta
 import pygame
@@ -11,29 +11,39 @@ set_default_color_theme("NightTrain.json")
 
 database.initialize_db()
 
+login_bg_data = Image.open("login_bg.jpg")
+email_icon_data = Image.open("email-icon.png")
+password_icon_data = Image.open("password-icon.png")
+google_icon_data = Image.open("google-icon.png")
+
+login_bg_image = CTkImage(dark_image=login_bg_data, light_image=login_bg_data, size=(300, 480))
+email_icon = CTkImage(dark_image=email_icon_data, light_image=email_icon_data, size=(20, 20))
+password_icon = CTkImage(dark_image=password_icon_data, light_image=password_icon_data, size=(17, 17))
+google_icon = CTkImage(dark_image=google_icon_data, light_image=google_icon_data, size=(17, 17))
+
 # Sign-up window
 def signup_window():
     signup_win = CTkToplevel()
     signup_win.geometry("300x500")
     signup_win.resizable(0, 0)
 
-    signup_label = CTkLabel(master=signup_win, text="Sign Up", font=("Arial", 20))
-    signup_label.pack(pady=10)
+    CTkLabel(master=signup_win, text="  Username", text_color="#601E88", anchor="w", justify="left", font=("Arial", 14, "bold"),
+             image=email_icon, compound="left").pack(anchor="w", pady=(38, 0), padx=(25, 0))
+    username_entry = CTkEntry(master=signup_win, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=2, text_color="#000000")
+    username_entry.pack(anchor="w", padx=(25, 0))
 
-    username_label = CTkLabel(master=signup_win, text="Username:", font=("Arial", 14))
-    username_label.pack(pady=5)
-    username_entry = CTkEntry(master=signup_win)
-    username_entry.pack(pady=5)
+    # password label and entry
+    CTkLabel(master=signup_win, text="  Password", text_color="#601E88", anchor="w", justify="left",
+             font=("Arial", 14, "bold"), image=password_icon, compound="left").pack(anchor="w", pady=(21, 0),padx=(25, 0))
+    password_entry = CTkEntry(master=signup_win, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=2,
+                              text_color="#000000", show="*")
+    password_entry.pack(anchor="w", padx=(25, 0))
 
-    password_label = CTkLabel(master=signup_win, text="Password:", font=("Arial", 14))
-    password_label.pack(pady=5)
-    password_entry = CTkEntry(master=signup_win, show="*")
-    password_entry.pack(pady=5)
-
-    confirm_password_label = CTkLabel(master=signup_win, text="Confirm Password:", font=("Arial", 14))
-    confirm_password_label.pack(pady=5)
-    confirm_password_entry = CTkEntry(master=signup_win, show="*")
-    confirm_password_entry.pack(pady=5)
+    CTkLabel(master=signup_win, text="  Confirm Password", text_color="#601E88", anchor="w", justify="left",
+             font=("Arial", 14, "bold"), image=password_icon, compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
+    confirm_password_entry = CTkEntry(master=signup_win, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=2,
+                              text_color="#000000", show="*")
+    confirm_password_entry.pack(anchor="w", padx=(25, 0))
 
     def register_user():
         username = username_entry.get()
@@ -51,8 +61,9 @@ def signup_window():
         else:
             messagebox.showerror("Error", "Please fill in all fields")
 
-    signup_button = CTkButton(master=signup_win, text="Sign Up", command=register_user)
-    signup_button.pack(pady=20)
+    signup_button = CTkButton(master=signup_win, text="Sign Up", fg_color="#601E88", hover_color="#E44982",
+                              font=("Arial", 12, "bold"), text_color="#ffffff", width=225, command=register_user).pack(anchor="w",
+                              pady=(10, 0), padx=(25, 0))
 
     signup_win.mainloop()
 
@@ -60,21 +71,34 @@ def signup_window():
 # Login window
 def login_window():
     login_win = CTk()
-    login_win.geometry("300x400")
+    login_win.geometry("600x480")
     login_win.resizable(0, 0)
 
-    login_label = CTkLabel(master=login_win, text="Login", font=("Arial", 20))
-    login_label.pack(pady=10)
+    CTkLabel(master=login_win, text="", image=login_bg_image).pack(expand=True, side="left")
 
-    username_label = CTkLabel(master=login_win, text="Username:", font=("Arial", 14))
-    username_label.pack(pady=5)
-    username_entry = CTkEntry(master=login_win)
-    username_entry.pack(pady=5)
+    # login right frame
+    frame = CTkFrame(master=login_win, width=300, height=480, fg_color="#ffffff", bg_color="#ffffff")
+    frame.pack_propagate(0)
+    frame.pack(expand=True, side="right")
 
-    password_label = CTkLabel(master=login_win, text="Password:", font=("Arial", 14))
-    password_label.pack(pady=5)
-    password_entry = CTkEntry(master=login_win, show="*")
-    password_entry.pack(pady=5)
+    CTkLabel(master=frame, text="Welcome Back!", text_color="#601E88", anchor="w", justify="left",
+             font=("Arial", 24, "bold")).pack(anchor="w", pady=(50, 5), padx=(25, 0))
+    CTkLabel(master=frame, text="Sign in to your account", text_color="#7E7E7E", anchor="w", justify="left",
+             font=("Arial", 12, "bold")).pack(anchor="w", padx=(25, 0))
+
+    # username label and entry
+    CTkLabel(master=frame, text="  Username", text_color="#601E88", anchor="w", justify="left", font=("Arial", 14, "bold"),
+             image=email_icon, compound="left").pack(anchor="w", pady=(38, 0), padx=(25, 0))
+    username_entry = CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=2,
+             text_color="#000000")
+    username_entry.pack(anchor="w", padx=(25, 0))
+
+    # password label and entry
+    CTkLabel(master=frame, text="  Password", text_color="#601E88", anchor="w", justify="left",
+             font=("Arial", 14, "bold"), image=password_icon, compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
+    password_entry = CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=2, text_color="#000000",
+             show="*")
+    password_entry.pack(anchor="w", padx=(25, 0))
 
     def attempt_login():
         username = username_entry.get()
@@ -85,11 +109,14 @@ def login_window():
         else:
             messagebox.showerror("Login Failed", "Invalid username or password")
 
-    login_button = CTkButton(master=login_win, text="Login", command=attempt_login)
-    login_button.pack(pady=20)
+    CTkButton(master=frame, text="Login", fg_color="#601E88", hover_color="#E44982", font=("Arial", 12, "bold"),
+              text_color="#ffffff", width=225, command=attempt_login).pack(anchor="w", pady=(40, 0), padx=(25, 0))
 
-    signup_button = CTkButton(master=login_win, text="Sign Up", command=signup_window)
-    signup_button.pack(pady=20)
+    CTkLabel(master=frame, text="Don't have an account?", text_color="#7E7E7E", anchor="w", justify="left",
+             font=("Arial", 12, "bold")).pack(anchor="w",pady=(30, 0), padx=(25, 0))
+
+    signup_button = CTkButton(master=frame, text="Sign Up", fg_color="#601E88", hover_color="#E44982", font=("Arial", 12, "bold"),
+              text_color="#ffffff", width=225, command=signup_window).pack(anchor="w", pady=(10, 0), padx=(25, 0))
 
     login_win.mainloop()
 
@@ -125,14 +152,6 @@ def main_window(username):
 
     CTkButton(master=left_frame, image=analytics_img, text="Dashboard", fg_color="transparent", font=("Arial Bold", 14),
               hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(60, 0))
-
-    # ... butonu
-    package_img_data = Image.open("package_icon.png")
-    package_img = CTkImage(dark_image=package_img_data, light_image=package_img_data)
-
-    CTkButton(master=left_frame, image=package_img, text="Orders", fg_color="transparent", font=("Arial Bold", 14),
-              text_color="white",
-              hover_color="#eee", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
 
     # ...2 butonu
     list_img_data = Image.open("list_icon.png")
@@ -198,14 +217,14 @@ def main_window(username):
     remove_button.place(x=360, y=270)
 
     # Görev çerçevesi
-    checkbox_frame = CTkScrollableFrame(master=app, border_color="MediumPurple3", border_width=2, width=200, height=350,
+    checkbox_frame = CTkScrollableFrame(master=app, fg_color="black", border_color="white", border_width=2, width=500, height=200,
                                         corner_radius=5)
-    checkbox_frame.place(x=600, y=200)
+    checkbox_frame.place(x=280, y=400)
 
     # Yapılan iş kalan iş çerçevesi
     task_status_frame = CTkFrame(master=app, border_color="MediumPurple3", border_width=1, width=300, height=100,
                                  corner_radius=4)
-    task_status_frame.place(x=280, y=340)
+    task_status_frame.place(x=570, y=140)
 
     # Tik işareti ikonu
     tik_image_data = Image.open("checked-svgrepo-com.png")
@@ -225,7 +244,7 @@ def main_window(username):
     # Success frame
     success_frame = CTkFrame(master=app, border_color="DeepSkyBlue2", border_width=1, width=300, height=100,
                              corner_radius=4)
-    success_frame.place(x=280, y=460)
+    success_frame.place(x=570, y=260)
 
     # Success ikonu
     success_image_data = Image.open("success.png")
@@ -247,7 +266,7 @@ def main_window(username):
     user_id = database.get_user_id(username)
     tasks = database.get_tasks_from_db(user_id)
     # Kullanıcının görevlerini burada yükleyip ekranda gösterebilirsiniz
-    for task_id, task_text, task_status in tasks:
+    for task_id, task_text, task_status, task_date, is_favorite in tasks:
         var = IntVar(value=task_status)
         checkbox = CTkCheckBox(master=checkbox_frame, text=task_text, variable=var, command=lambda: database.update_task_status_in_db(task_id, var.get()))
         checkbox.pack(anchor="w", padx=10, pady=5)
@@ -256,12 +275,12 @@ def main_window(username):
     def update_task_status():
         global previous_tasks_done
         tasks_done = sum(1 for task_id, var in checkboxes if var.get() == 1)
-        tasks_remaining = len(task_list) - tasks_done
+        tasks_remaining = len(checkboxes) - tasks_done
         tasks_done_label.configure(text=f"Tasks Done: {tasks_done}")
         tasks_remaining_label.configure(text=f"Tasks Remaining: {tasks_remaining}")
 
-        if len(task_list) > 0:
-            success_percentage = int((tasks_done / len(task_list)) * 100)
+        if len(checkboxes) > 0:
+            success_percentage = int((tasks_done / len(checkboxes)) * 100)
         else:
             success_percentage = 0
         success_percentage_value_label.configure(text=f"{success_percentage}%")
@@ -298,31 +317,23 @@ def main_window(username):
 
         CTkButton(master=calendar_window, text="Select Date", command=select_date).pack(pady=10)
 
-    def add_task(task):
+    def add_task(task_id, task_text, due_date):
         var = IntVar()
-        checkbox = CTkCheckBox(master=checkbox_frame, text=task, variable=var, command=update_task_status)
+        checkbox_text = f"{task_text}   | {due_date} |" if due_date != "No date selected" else task_text
+        checkbox = CTkCheckBox(master=checkbox_frame, text=checkbox_text, variable=var, command=update_task_status)
         checkbox.pack(anchor="w", padx=10, pady=5)
-        tasks.append(task)
-        checkboxes.append(var)
+        checkboxes.append((task_id, var))
         update_task_status()
 
     def add_task_window():
         new_task_window = CTkToplevel(app)
-        new_task_window.geometry("300x250")
+        new_task_window.geometry("300x200")
         new_task_window.resizable(0, 0)
 
-        app_x = app.winfo_x()
-        app_y = app.winfo_y()
-        app_width = app.winfo_width()
-        app_height = app.winfo_height()
+        new_x = app.winfo_x() + (app.winfo_width() - 300) // 2
+        new_y = app.winfo_y() + (app.winfo_height() - 200) // 2
 
-        new_window_width = 300
-        new_window_height = 250
-
-        new_x = app_x + (app_width - new_window_width) // 2
-        new_y = app_y + (app_height - new_window_height) // 2
-
-        new_task_window.geometry(f"{new_window_width}x{new_window_height}+{new_x}+{new_y}")
+        new_task_window.geometry(f"300x350+{new_x}+{new_y}")
         new_task_window.transient(app)
         new_task_window.grab_set()
 
@@ -332,51 +343,39 @@ def main_window(username):
         task_entry = CTkEntry(master=new_task_window, placeholder_text="Enter a task...", width=250)
         task_entry.pack(pady=5)
 
+        selected_due_date = StringVar()
+        selected_due_date.set("No date selected")
+
+        is_favorite_var = IntVar(value=0)
+        CTkRadioButton(master=new_task_window, text="Add To Favorites", variable=is_favorite_var, value=1).pack(pady=5)
+        CTkRadioButton(master=new_task_window, text="Normal", variable=is_favorite_var, value=0).pack(pady=5)
+
         def open_calendar():
             calendar_window = CTkToplevel(app)
             calendar_window.geometry("300x300")
             calendar_window.resizable(0, 0)
 
-            calendar_window.geometry(f"{300}x{300}+{new_x}+{new_y}")
-            calendar_window.transient(app)
-            calendar_window.grab_set()
-
-            today = datetime.today()
-            cal = Calendar(calendar_window, selectmode='day', year=today.year, month=today.month, day=today.day,
-                           date_pattern='dd.mm.yyyy')
+            cal = Calendar(calendar_window, selectmode='day', year=datetime.today().year,
+                           month=datetime.today().month, day=datetime.today().day, date_pattern='dd.mm.yyyy')
             cal.pack(pady=20)
 
             def select_date():
                 selected_date = cal.selection_get()
-                selected_due_date.set(selected_date.strftime("%d.%m.%Y"))
+                selected_due_date.set(selected_date.strftime("%Y-%m-%d"))  # Format YYYY-MM-DD
                 calendar_window.destroy()
 
-            select_button = CTkButton(master=calendar_window, text="Select Date", command=select_date)
-            select_button.pack(pady=10)
-
-        selected_due_date = StringVar()
-        selected_due_date.set("No date selected")
-
-        def save_task():
-            task_name = task_entry.get()
-            if task_name:
-                database.add_task_to_db(user_id, task_name)
-                new_task_window.destroy()
-                refresh_task_list()
-
-        save_button = CTkButton(master=new_task_window, text="Save Task", command=save_task)
-        save_button.pack(pady=20)
+            CTkButton(master=calendar_window, text="Select Date", command=select_date).pack(pady=10)
 
         date_options = ["No Due Date", "Today", "Tomorrow", "Next Week", "Pick a Date"]
         selected_date_option = StringVar(value=date_options[0])
 
         def handle_date_option(choice):
             if choice == "Today":
-                selected_due_date.set(datetime.today().strftime("%d.%m.%Y"))
+                selected_due_date.set(datetime.today().strftime("%Y-%m-%d"))
             elif choice == "Tomorrow":
-                selected_due_date.set((datetime.today() + timedelta(days=1)).strftime("%d.%m.%Y"))
+                selected_due_date.set((datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d"))
             elif choice == "Next Week":
-                selected_due_date.set((datetime.today() + timedelta(weeks=1)).strftime("%d.%m.%Y"))
+                selected_due_date.set((datetime.today() + timedelta(weeks=1)).strftime("%Y-%m-%d"))
             elif choice == "Pick a Date":
                 open_calendar()
 
@@ -387,22 +386,34 @@ def main_window(username):
         selected_date_label = CTkLabel(master=new_task_window, textvariable=selected_due_date)
         selected_date_label.pack(pady=5)
 
+        def save_task():
+            task_name = task_entry.get()
+            due_date = selected_due_date.get()
+            is_favorite = is_favorite_var.get()
+            if task_name:
+                task_id = database.add_task_to_db(user_id, task_name, due_date, is_favorite)
+                add_task(task_id, task_name, due_date)
+                refresh_task_list()
+                update_task_status()
+                new_task_window.destroy()
+
         save_button = CTkButton(master=new_task_window, text="Save Task", command=save_task)
         save_button.pack(pady=20)
 
     def refresh_task_list():
-        # Mevcut checkbox'ları temizle
         for widget in checkbox_frame.winfo_children():
             widget.destroy()
 
-        # Görev listesini ve checkbox'ları yeniden doldur
         global task_list
         task_list = database.get_tasks_from_db(user_id)
-        checkboxes.clear()  # Önceki checkbox'ları temizle
+        checkboxes.clear()
 
-        for task_id, task_text, task_status in task_list:
+        for task_id, task_text, task_status, selected_due_date, is_favorite in task_list:
+            display_text = f"{task_text} (Due: {selected_due_date})" if selected_due_date else task_text
+            if is_favorite:
+                display_text += " ★"
             var = IntVar(value=task_status)
-            checkbox = CTkCheckBox(master=checkbox_frame, text=task_text, variable=var, command=update_task_status)
+            checkbox = CTkCheckBox(master=checkbox_frame, text=display_text, variable=var, command=update_task_status)
             checkbox.pack(anchor="w", padx=10, pady=5)
             checkboxes.append((task_id, var))
 
@@ -425,35 +436,26 @@ def main_window(username):
         remove_frame.pack(pady=10)
 
         remove_vars = []
-        for task, checkbox in zip(tasks, checkboxes):
+        for task_id, task_text, task_status, due_date, is_favorite in task_list:
             var = IntVar()
-            remove_checkbox = CTkCheckBox(master=remove_frame, text=task, variable=var)
+            remove_checkbox = CTkCheckBox(master=remove_frame, text=task_text, variable=var)
             remove_checkbox.pack(anchor="w", padx=10, pady=5)
-            remove_vars.append((task, var))
+            remove_vars.append((task_id, var))
 
         def remove_selected_tasks():
-            global tasks, checkboxes
-            new_tasks = []
-            new_checkboxes = []
+            global task_list, checkboxes
+            task_ids_to_remove = [task_id for task_id, var in remove_vars if var.get() == 1]
 
-            task_ids_to_remove = [task_id for (task_id, _), (remove_task, remove_var) in zip(checkboxes, remove_vars) if
-                                  remove_var.get() == 1]
-
-            # Veritabanından görevleri sil
             for task_id in task_ids_to_remove:
                 database.remove_task_from_db(task_id)
 
-            # Görev listesini güncelle
-            for (task_id, checkbox_loop), (remove_task, remove_var) in zip(checkboxes, remove_vars):
-                if remove_var.get() == 0:
-                    new_tasks.append(task_id)
-                    new_checkboxes.append(checkbox_loop)
-
-            tasks = new_tasks
-            checkboxes = new_checkboxes
+            refresh_task_list()
             update_task_status()
             remove_window.destroy()
-            refresh_task_list()
+
+        remove_button = CTkButton(master=remove_window, text="Remove Selected Tasks", command=remove_selected_tasks)
+        remove_button.pack(pady=10)
+
 
         CTkButton(master=remove_window, text="Remove Selected Tasks", command=remove_selected_tasks).pack(pady=10)
 
