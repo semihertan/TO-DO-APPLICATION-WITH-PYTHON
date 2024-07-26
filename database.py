4f41f560d27241a4d93e2c2d73a1d018089eca5d
Semih ERTAN <161311067+semihertan@users.noreply.github.com>
2024-07-26 17:50:23 +0300

    Update database.py

--- database.py
+++ database.py
@@ -17,7 +17,6 @@ def initialize_db():
                       status TEXT NOT NULL,
                       is_favorite INTEGER default 0,
                       due_date TEXT,
-                      reminder_time TEXT,
                       FOREIGN KEY (user_id) REFERENCES users(id))''')
     conn.commit()
     conn.close()
@@ -43,13 +42,12 @@ def check_credentials(username, password):
     conn.close()
     return result
 
-def add_task_to_db(user_id, task, is_favorite, due_date, reminder_time=None):
+def add_task_to_db(user_id, task, is_favorite, due_date):
     due_date_str = due_date.get() if isinstance(due_date, StringVar) else due_date
-    reminder_time_str = reminder_time.get() if isinstance(reminder_time, StringVar) else reminder_time
     conn = sqlite3.connect("user_data.db")
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO tasks (user_id, task, status, is_favorite, due_date, reminder_time) VALUES (?, ?, ?, ?, ?, ?)",
-                   (user_id, task, 0, is_favorite, due_date_str, reminder_time_str))
+    cursor.execute("INSERT INTO tasks (user_id, task, status, is_favorite, due_date) VALUES (?, ?, ?, ?, ?)",
+                   (user_id, task, 0, is_favorite, due_date_str))
     conn.commit()
     conn.close()
 
@@ -86,7 +84,8 @@ def get_user_id(username):
 def get_past_due_tasks(user_id):
     conn = sqlite3.connect("user_data.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT task, due_date FROM tasks WHERE user_id = ? AND due_date < ?", (user_id, datetime.now()))
+    now = datetime.now().strftime('%d.%m.%Y')
+    cursor.execute("SELECT task, due_date FROM tasks WHERE user_id = ? AND due_date < ?", (user_id, now))
     past_due_tasks = cursor.fetchall()
     conn.close()
     return past_due_tasks
@@ -94,7 +93,7 @@ def get_past_due_tasks(user_id):
 def check_reminders_and_notify():
     conn = sqlite3.connect("user_data.db")
     cursor = conn.cursor()
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    now = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
 
     cursor.execute("SELECT id, task, remimder_time FROM tasks WHERE reminder_time = ?", (now,))
     tasks = cursor.fetchall()
